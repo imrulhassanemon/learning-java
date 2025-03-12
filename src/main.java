385667c646d893fad042670b959cfbd3a949995b
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-03-12 09:04:41 +0600

    the first commit I pushed for java program

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args){
+
+        // System.out.print("hello world");
+
+        System.out.println("Hello world ");
+        System.out.println("I like pizza. It is really good.");
+        System.out.println("By me pizza");
+
+    }
+}
